00271431c0065b16c86f90e730c33965a3ad1e15
anguojian <13261712121@163.com>
2023-05-14 14:16:05 +0800

    1

--- src/cn/tedu/collection/Point.java
+++ src/cn/tedu/collection/Point.java
@@ -31,6 +31,7 @@ public class Point {
     public String toString() {
         return "(" + x + ", " + y + ")";
     }
+
     //alt+insert→equals and hashcode→Next→Next→Finish
     @Override
     public boolean equals(Object o) {
