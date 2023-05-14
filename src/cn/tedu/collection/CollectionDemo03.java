1d37d53ad5471c0e174a351cf8cad821de454e64
anguojian <13261712121@163.com>
2023-05-14 16:12:21 +0800

    1

--- src/cn/tedu/collection/CollectionDemo03.java
+++ src/cn/tedu/collection/CollectionDemo03.java
@@ -3,22 +3,54 @@ package cn.tedu.collection;
 import java.util.ArrayList;
 import java.util.Collection;
 
+/**
+ * 集合间的操作
+ */
 public class CollectionDemo03 {
     public static void main(String[] args) {
-        Collection c = new ArrayList();
-        Point p = new Point(1, 2);
-        c.add(p);
-        System.out.println("p = " + p);
-        System.out.println("c = " + c);
-        p.setX(2);
-        System.out.println("p = " + p);
-        System.out.println("c = " + c);
-        dosome();
-    }
-
-    private static void dosome() {
-        int a = 1;
-        int b = 1;
-        int c = 1;
+        Collection c1 = new ArrayList();
+        c1.add("大娃");
+        c1.add("二娃");
+        c1.add("三娃");
+        System.out.println("c1 = " + c1);
+        Collection c2 = new ArrayList();
+        c2.add("四娃");
+        c2.add("五娃");
+        //c2.add("大娃");
+        System.out.println("c2 = " + c2);
+        /*
+         * boolean addAll(Collection c);
+         * 将给定集合中的所有元素都添加到当前集合中(取并集)
+         * 操作后,如果当前集合发生了改变,则返回true
+         */
+        c1.addAll(c2);
+        System.out.println("c1 = " + c1);
+        System.out.println("c2 = " + c2);
+        Collection c3 = new ArrayList();
+        c3.add("二娃");
+        c3.add("大娃");
+        c3.add("七娃");
+        System.out.println("c3 = " + c3);
+        /*
+         * boolean containsAll(Collection c);
+         * 判断当前集合中是否包含给定集合中的所有元素
+         */
+        boolean b = c1.containsAll(c3);
+        System.out.println("c1集合是否包含c3集合:" + b);
+        /*
+         * boolean retainAll(Collection c);
+         * 将当前集合中的元素保留和给定集合中的元素相同的部分(取交集)
+         */
+        c1.retainAll(c3);
+        System.out.println("c1 = " + c1);
+        System.out.println("c3 = " + c3);
+        c1.add("蛇精");
+        /*
+         * boolean removeAll(Collection c);
+         * 将当前集合中和给定集合中共有的元素删除
+         */
+        c1.removeAll(c3);
+        System.out.println("c1 = " + c1);
+        System.out.println("c3 = " + c3);
     }
 }
--- 课程资料/API部分笔记MD版/PART06-集合.md
+++ 课程资料/API部分笔记MD版/PART06-集合.md
@@ -234,7 +234,62 @@ public class Point {
 ### 1.7 CollectionDemo3
 
 ```java
+package cn.tedu.collection;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
+/**
+ * 集合间的操作
+ */
+public class CollectionDemo03 {
+    public static void main(String[] args) {
+        Collection c1 = new ArrayList();
+        c1.add("大娃");
+        c1.add("二娃");
+        c1.add("三娃");
+        System.out.println("c1 = " + c1);
+        Collection c2 = new ArrayList();
+        c2.add("四娃");
+        c2.add("五娃");
+        //c2.add("大娃");
+        System.out.println("c2 = " + c2);
+        /*
+         * boolean addAll(Collection c);
+         * 将给定集合中的所有元素都添加到当前集合中(取并集)
+         * 操作后,如果当前集合发生了改变,则返回true
+         */
+        c1.addAll(c2);
+        System.out.println("c1 = " + c1);
+        System.out.println("c2 = " + c2);
+        Collection c3 = new ArrayList();
+        c3.add("二娃");
+        c3.add("大娃");
+        c3.add("七娃");
+        System.out.println("c3 = " + c3);
+        /*
+         * boolean containsAll(Collection c);
+         * 判断当前集合中是否包含给定集合中的所有元素
+         */
+        boolean b = c1.containsAll(c3);
+        System.out.println("c1集合是否包含c3集合:" + b);
+        /*
+         * boolean retainAll(Collection c);
+         * 将当前集合中的元素保留和给定集合中的元素相同的部分(取交集)
+         */
+        c1.retainAll(c3);
+        System.out.println("c1 = " + c1);
+        System.out.println("c3 = " + c3);
+        c1.add("蛇精");
+        /*
+         * boolean removeAll(Collection c);
+         * 将当前集合中和给定集合中共有的元素删除
+         */
+        c1.removeAll(c3);
+        System.out.println("c1 = " + c1);
+        System.out.println("c3 = " + c3);
+    }
+}
 ```
 
 ### 1.8 CollectionDemo4
