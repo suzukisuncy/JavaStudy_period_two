e8281a011c050931ad2f851a1968f79ee112b85d
anguojian <13261712121@163.com>
2023-05-14 16:59:12 +0800

    1

--- src/cn/tedu/collection/CollectionDemo03.java
+++ src/cn/tedu/collection/CollectionDemo03.java
@@ -44,7 +44,7 @@ public class CollectionDemo03 {
         c1.retainAll(c3);
         System.out.println("c1 = " + c1);
         System.out.println("c3 = " + c3);
-        c1.add("爷爷");
+        c1.add("小明");
         /*
          * boolean removeAll(Collection c);
          * 将当前集合中和给定集合中共有的元素删除
